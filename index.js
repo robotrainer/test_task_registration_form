d4fefe3ab4961bd1fa86cb9d5acec3a7a3b26201
robotrainer <asbu26@gmail.com>
2021-11-03 17:57:31 +0700

    validation birthDate

--- index.js
+++ index.js
@@ -37,11 +37,15 @@ app.get("/", (req, res) => {
   }
   else if (req.session.passwordMissmath) {
     delete req.session.passwordMissmath;
-    html = html.replace(`{passwordMissmath}`, `<div class="row invalid">Минимальная длина пароля 8 символов. Пароль должен содержать минимум одну цифру, по одной заглавной и строчную буквы и один символ, например, !@#$%^&*</div>`);
+    html = html.replace(`{passwordMissmath}`, `<div class="row invalid">Минимальная длина пароля 8 символов. Пароль должен содержать минимум одну цифру, по одной заглавной и строчную буквы и один символ, например, !@#$%^&*-_</div>`);
   }
   else if (req.session.passwordsMissmath) {
     delete req.session.passwordsMissmath;
     html = html.replace(`{passwordMissmath}`, `<div class="row invalid">Пароли не совпадают</div>`);
+  }
+  else if (req.session.birthDateMissmath) {
+    delete req.session.birthDateMissmath;
+    html = html.replace(`{birthDateMissmath}`, `<div class="row invalid">Вам нет ещё 18 лет</div>`);
   }
   // else {
   //   html = html.replace(/\{[a-zA-Z]*\}/g, ``);
@@ -57,30 +61,50 @@ app.post("/", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const passwordRepeat = req.body.passwordRepeat;
+  const birthDate = req.body.birthDate.split("-");
+  const birthDay = birthDate[2];
+  const birthMonth = birthDate[1] - 1;
+  const birthYear = birthDate[0];
+  const age = 18;
+
+  let mydate = new Date();
+  mydate.setFullYear(birthYear, birthMonth, birthDay);
+  let nowDate = new Date();
+  let setDate = new Date();
+  setDate.setFullYear(mydate.getFullYear() + age, birthMonth, birthDay);
 
   const firstNameRegExp = /^(?=.{2,255}$)[A-Za-zА-Яа-яЁё]/;
   const lastNameRegExp = /^(?=.{1,255}$)[A-Za-zА-Яа-яЁё]/;
   const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  const passwordRegExp = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g;
+  const passwordRegExp = /(?=.*[0-9])(?=.*[!@#$%^&*-_])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g;
 
   if (!firstNameRegExp.test(firstName)) {
     req.session.firstNameMissmath = true;
     res.redirect('/');
+    return;
   }
   else if (!lastNameRegExp.test(lastName)) {
     req.session.lastNameMissmath = true;
     res.redirect('/');
+    return;
   }
   else if (!emailRegExp.test(email)) {
     req.session.emailMissmath = true;
     res.redirect('/');
+    return;
   }
   else if (!passwordRegExp.test(password)) {
     req.session.passwordMissmath = true;
-    res.redirect('/')
+    res.redirect('/');
+    return;
   }
   else if (password != passwordRepeat) {
     req.session.passwordsMissmath = true;
+    res.redirect('/');
+    return;
+  }
+  else if ((nowDate - setDate) < 0) {
+    req.session.birthDateMissmath = true;
     res.redirect('/');
     return;
   }
